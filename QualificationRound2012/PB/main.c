9dcbf9fd996d44c99e859a825756494bc35c89e2
Haotian Liu <liuhaotian.air@gmail.com>
2012-04-13 21:52:05 -0500

    Finish the small set!!!

--- QualificationRound2012/PB/main.c
+++ QualificationRound2012/PB/main.c
@@ -11,35 +11,58 @@ int main(int argc, char const *argv[])
 	fscanf(fin, "%d\n", &T);
 
 	char in[400];
-	char out[400];
+	int out;
 	char ocase[20];
+
+	int n3p43p3, n3p2;
+	//int n2930, n01, nunclear, n3p43p3, n3p4, n3p2, n3p1, n3p3;
+	// n2930 the # of 29 and 30, n01 the # of 0 and 1
+	//
 
 	for (i = 0; i < T; ++i)
 	{
 		fscanf(fin, "%d ", &N);
 		fscanf(fin, "%d ", &S);
 		fscanf(fin, "%d ", &p);
+
+		n3p2 = 0;
+		n3p43p3 = 0;
+
 		for (j = 0; j < N - 1; ++j)
 		{
 			fscanf(fin, " %d", &(t[j]));
 		}
 		fscanf(fin, " %d\n", &(t[j]));
-		printf("%s\n", (char *)t);
+
+		for (j = 0; j < N; ++j)
+		{
+			if (t[j] >= 3*p - 2)
+				n3p2++;
+			if (p == 1)
+			{
+				n3p43p3 = 0;
+			}
+			else if ((t[j] == (3*p - 4)) || (t[j] == (3*p - 3)))
+				n3p43p3++;
+		}
+
+		if (S < n3p43p3)
+			out = S + n3p2;
+		else if (S >= n3p43p3)
+			out = n3p43p3 + n3p2;
 
 		//memset(ocase, 0, 20);
 		//memset(in, 0, 120);
 		//memset(out, 0, 120);
-		sprintf(ocase, "Case #%d: ", i+1);
-		strcpy(out, ocase);
-
-		/*
+		sprintf(ocase, "Case #%d: %d", i+1, out);
+		//strcpy(out, ocase);
+		
 		if (i != T -1)
 		{
-			fprintf (fout, "%s\n", out);
+			fprintf (fout, "%s\n", ocase);
 		}
 		else
-			fprintf (fout, "%s", out);
-		*/
+			fprintf (fout, "%s", ocase);
 	}
 
 	fclose(fin);
