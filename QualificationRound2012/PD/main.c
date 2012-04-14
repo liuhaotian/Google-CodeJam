c010a45fc97393e3b139b255d8baad22beb09ba8
Haotian Liu <liuhaotian.air@gmail.com>
2012-04-14 15:02:11 -0500

    Build the entire world!

--- QualificationRound2012/PD/main.c
+++ QualificationRound2012/PD/main.c
@@ -23,6 +23,10 @@ typedef struct
 	long long int Xx, Xy;
 } mirrorobj;
 
+void mirTheWall(mirrorobj* mirrorhall);
+
+void printMap(mirrorobj* mirrorhall);
+
 int main(int argc, char const *argv[])
 {
 	FILE * fin = fopen("input.in", "r");
@@ -41,10 +45,13 @@ int main(int argc, char const *argv[])
 	for (i = 0; i < T; ++i)
 	{
 		fscanf(fin, "%lld ", &H);
+		mirrorhall.H = H;
 		//printf("%lld\n", H);
 		fscanf(fin, "%lld ", &W);
+		mirrorhall.W = W;
 		//printf("%lld\n", W);
 		fscanf(fin, "%lld\n", &D);
+		mirrorhall.D = D;
 		//printf("%lld\n", D);
 
 		out = 0;
@@ -55,8 +62,35 @@ int main(int argc, char const *argv[])
 		{
 			fscanf(fin, "%s\n", mirrorhall.map + W * j );
 		}
+		for (j = 0; j < W; ++j)
+		{
+			for (k = 0; k < H; ++k)
+			{
+				if (mirrorhall.map[j + W * k] == 'X')
+				{
+					mirrorhall.Xx = j;
+					mirrorhall.Xy = k;
+				}
+			}
+		}
 		//printf("%s", mirrorhall.map);
+		//printMap(&mirrorhall);
+
 		//mirTheWall(mirrorhall);
+		
+		while(
+				((mirrorhall.Xx + 0.5 - 1) * 2 <= mirrorhall.D) ||
+				((mirrorhall.Xy + 0.5 - 1) * 2 <= mirrorhall.D) ||
+				(((mirrorhall.W - mirrorhall.Xx - 0.5 - 1)) * 2 <= mirrorhall.D) ||
+				(((mirrorhall.H - mirrorhall.Xy - 0.5 - 1)) * 2 <= mirrorhall.D)
+				){
+			mirTheWall(&mirrorhall);
+		}
+		//mirTheWall(&mirrorhall);
+
+
+		printMap(&mirrorhall);
+
 		//mirTheNode();
 		//travTheHall();
 
@@ -81,4 +115,126 @@ int main(int argc, char const *argv[])
 
 
 	return 0;
+}
+
+void mirTheWall(mirrorobj* mirrorhall){
+	long long int W = (*mirrorhall).W;
+	long long int H = (*mirrorhall).H;
+	long long int D = (*mirrorhall).D;
+	long long int newW = 3 * W - 4;
+	long long int newH = 3 * H - 4;
+
+	long long int i, X0, Y0;
+	long long int j, k, newi, newj, oldi, oldj;
+	long long int Xx, Xy;
+	Xx = (*mirrorhall).Xx + W - 2;
+	Xy = (*mirrorhall).Xy + H - 2;
+
+	char* newmap = calloc(newH * newW + 1, sizeof(char));
+	memset(newmap, 'U', newW * newH);
+
+	// copy to the main
+	for (i = 0; i < W; ++i)
+	{
+		oldi = i;
+		newi = W - 2 + i;
+
+		for (j = 0; j < H; ++j)
+		{
+			oldj = j;
+			newj = H - 2 + j;
+
+			newmap[newi + newW * newj] = (*mirrorhall).map[i + j * W];
+		}
+	}
+
+	// copy to the left
+	X0 = W * 2 - 3;
+	for (i = 0; i < W - 1; ++i)
+	{
+		oldi = i + W - 1;
+		newi = X0 - oldi;
+		for (j = 0; j < newH; ++j)
+		{
+			oldj = j;
+			newj = j;
+			newmap[newi + newW * newj] = newmap[oldi + newW * oldj];
+		}
+	}
+
+	// copy to the right
+	//X0 = W * 2 - 1;
+	for (i = 0; i < W - 1; ++i)
+	{
+		oldi = i;
+		newi = newW - oldi - 1;
+		for (j = 0; j < H; ++j)
+		{
+			oldj = j;
+			newj = H - 2 + j;
+			newmap[newi + newW * newj] = (*mirrorhall).map[oldi + W * oldj];
+		}
+	}
+
+	// copy to the up
+	Y0 = H * 2 - 3;
+	for (i = 0; i < newW; ++i)
+	{
+		oldi = i;
+		newi = i;
+		for (j = 0; j < H - 1; ++j)
+		{
+			oldj = j + H - 1;
+			newj = Y0 - oldj;
+			newmap[newi + newW * newj] = newmap[oldi + newW * oldj];
+		}
+	}
+
+	// copy to the up
+	Y0 = H * 4 - 7;
+	for (i = 0; i < newW; ++i)
+	{
+		oldi = i;
+		newi = i;
+		for (j = 0; j < H - 1; ++j)
+		{
+			oldj = j + H - 1;
+			newj = Y0 - oldj;
+			newmap[newi + newW * newj] = newmap[oldi + newW * oldj];
+		}
+		newmap[newi + newW * (newH - 1)] = '#';
+	}
+
+	free((*mirrorhall).map);
+	(*mirrorhall).map = newmap;
+	(*mirrorhall).W = newW;
+	(*mirrorhall).H = newH;
+	(*mirrorhall).Xx = Xx;
+	(*mirrorhall).Xy = Xy;
+
+	//printf("%c\n", newmap[Xx + newW * Xy]);
+}
+
+void printMap(mirrorobj* mirrorhall){
+	long long int W = (*mirrorhall).W;
+	long long int H = (*mirrorhall).H;
+	long long int D = (*mirrorhall).D;
+	char* map = (*mirrorhall).map;
+
+	char* tmpline = calloc(W + 1, sizeof(char));
+
+	long long int i;
+
+	//printf("%lld %lld %lld\n", H, W, D);
+	//printf("%s\n", map);
+	for (i = 0; i < H; ++i)
+	{
+		memcpy(tmpline, map + W * i, W);
+		tmpline[W] = 0;
+		printf("%s\n", tmpline);
+	}
+	//printf("%s\n", map);
+	printf("\n");
+
+	free(tmpline);
 }
