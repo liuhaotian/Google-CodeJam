ac798e217ed4b8832439de1e1c81d2b21a876863
Haotian Liu <liuhaotian.air@gmail.com>
2012-04-13 19:50:56 -0500

    Finish the printout

--- QualificationRound2012/PA/main.c
+++ QualificationRound2012/PA/main.c
@@ -3,12 +3,13 @@
 
 int main(int argc, char const *argv[])
 {
-	FILE * fin = fopen("input.in", "r");
-	FILE * fout = fopen("output.out", "r");
+	FILE * fin = fopen("refinput.in", "r");
+	FILE * fout = fopen("refoutput.out", "r");
 
 	int T, i, j;
 	char tab[27];
 	memset(tab, ' ', 26);
+	tab[26] = 0;
 	tab['q'-'a']='z';
 	tab['e'-'a']='o';
 	tab['y'-'a']='a';
@@ -18,10 +19,10 @@ int main(int argc, char const *argv[])
 	char out[120];
 	char ocase[20];
 	int offset;
+	char temp;
 	char * outp;
 
-	fscanf(fin, "%d", &T);
-	fgetc(fin);
+	fscanf(fin, "%d\n", &T);
 
 	for (i = 0; i < T; ++i)
 	{
@@ -34,15 +35,46 @@ int main(int argc, char const *argv[])
 		//printf("tste\n");
 		for (j = 0; in[j] != 0; ++j)
 		{
-			if(in[j] != ' '){
+			if( (in[j] != ' ') && (in[j] >='a') && (in[j] <= 'z') )
+			{
 				tab[in[j]-'a']=out[j + offset];
 			}
 		}
 	}
 
-	printf("%s\n", tab);
+	//printf("%s\n", tab);
 	//fscanf(fin, "%d", &T);
 	//printf("%d\n", T);
+	fclose(fin);
+	fclose(fout);
+
+	fin = fopen("input.in", "r");
+	fout = fopen("output.out", "w");
+	fscanf(fin, "%d\n", &T);
+
+	for (i = 0; i < T; ++i)
+	{
+		memset(ocase, 0, 20);
+		memset(in, 0, 100);
+		memset(out, 0, 120);
+		sprintf(ocase, "Case #%d: ", i);
+		strcpy(out, ocase);
+		fgets(in, 100, fin);
+		fscanf(fin, "\n", &temp);
+
+		offset = strlen(ocase);
+		for (j = 0; in[j] != 0; ++j)
+		{
+			if( (in[j] != ' ') && (in[j] >='a') && (in[j] <= 'z') )
+			{
+				out[j + offset] = tab[in[j]-'a'];
+			}
+			else
+				out[j + offset] = ' ';
+		}
+		printf("%s\n", out);
+	}
+
 	fclose(fin);
 	fclose(fout);
 
