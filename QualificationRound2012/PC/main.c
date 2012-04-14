eb9c55d98a7b7b5aec5a05d0384b6194c314c02a
Haotian Liu <liuhaotian.air@gmail.com>
2012-04-14 00:26:02 -0500

    Checkpoint

--- QualificationRound2012/PC/main.c
+++ QualificationRound2012/PC/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[])
 {
@@ -7,13 +8,17 @@ int main(int argc, char const *argv[])
 	FILE * fout = fopen("output.out", "w");
 	
 	int T;
-	long int A, B;
-	int i, j;
+	unsigned long int A, B;
+	int i;
+	long int j, k;
 	fscanf(fin, "%d\n", &T);
 
 	char in[40];
 	unsigned long long out;
 	char ocase[40];
+	char * tab;
+	unsigned long int tmp;
+	unsigned long int A0;
 
 	for (i = 0; i < T; ++i)
 	{
@@ -21,6 +26,30 @@ int main(int argc, char const *argv[])
 		//printf("%lu\n", A);
 		fscanf(fin, "%lu", &B);
 		//printf("%lu\n", B);
+		out = 0;
+
+		for (A0 = 1; A/A0 > 10; A0 *= 10)
+		{
+			;
+		}
+		//printf("%ld\n", A0);
+
+		tab = calloc(B, sizeof(char));
+		for (j = A; j <= B; ++j)
+		{
+			for (k = 10; k <= A0; ++k)
+			{
+				tmp = j / k + (j % k) * (A0 / k) * 10;
+				if(tmp < A)continue;
+				if(tmp > B)continue;
+				tab[ tmp ] = 1;
+			}
+		}
+		for (j = A; j <= B; ++j)
+		{
+			out += tab[i];
+		}
+		free(tab);
 
 		sprintf(ocase, "Case #%d: %llu", i+1, out);
 		
