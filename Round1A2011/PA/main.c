5968205c552010dbb8d0ec20369bd4b6e5d4e106
Haotian Liu <liuhaotian.air@gmail.com>
2012-04-27 19:33:09 -0500

    Finish

--- Round1A2011/PA/main.c
+++ Round1A2011/PA/main.c
@@ -3,6 +3,46 @@
 #include <stdlib.h>
 #include <math.h>
 
+long gcd(long a,long b)
+{
+	long c;
+	while(1)
+	{
+		c = a % b;
+		if(c == 0)
+			return b;
+		a = b;
+		b = c;
+	}
+}
+
+struct tab
+{
+	long nwin;
+	long nloss;
+} ptab[101];
+
+long calc(long N, long PD, long PG){
+	long ret = 0;
+	long D, G;
+
+	if (N < ptab[PD].nwin + ptab[PD].nloss)
+	{
+		return 0;
+	}
+	if (PG == 100 && PD != 100)
+	{
+		return 0;
+	}
+	if (PG == 0 && PD != 0)
+	{
+		return 0;
+	}
+
+	return 1;
+
+}
+
 int main(int argc, char const *argv[])
 {
 	FILE * fin = fopen("input.in", "r");
@@ -10,20 +50,31 @@ int main(int argc, char const *argv[])
 	
 	long T, N, PD, PG;
 	long i;
-	bool out;
-	char out_true = "Possible";
-	char out_false = "Broken";
+	long out;
+	char out_true[] = "Possible";
+	char out_false[] = "Broken";
 
 	fscanf(fin, "%ld\n", &T);
 	char ocase[40];
 
+	for (i = 1; i < 100; ++i)
+	{
+		ptab[i].nwin = i / gcd(i, 100 - i);
+		ptab[i].nloss = (100 - i) / gcd(i, 100 - i);
+	}
+	ptab[0].nwin = 0;
+	ptab[0].nloss = 1;
+
+	ptab[100].nwin = 1;
+	ptab[100].nloss = 0;
+
 	for (i = 0; i < T; ++i)
 	{
-		memset(time_string, 0, 80);
-		fscanf(fin, "%s\n", time_string);
-		//printf("%s\n", time_string);
+		fscanf(fin, "%ld %ld %ld\n", &N, &PD, &PG);
+		//printf("%ld %ld %ld\n", N, PD, PG);
 		
 		//out = mintime(time_string);
+		out = calc(N, PD, PG);
 
 		sprintf(ocase, "Case #%ld: %s", i + 1, out?out_true:out_false);
 		//printf("%s\n", ocase);
