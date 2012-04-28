9c45663ed32f5ac0136b5cd17fdfbcae5bccf935
Haotian Liu <liuhaotian.air@gmail.com>
2012-04-27 22:20:04 -0500

    Find max

--- Round1A2012/PB/main.c
+++ Round1A2012/PB/main.c
@@ -5,6 +5,20 @@
 
 long gcd(long a,long b){long c;while(1){c = a % b;if(c == 0)return b;a = b;b = c;}}
 
+long findmaxb(long * req, long N){
+	long i;
+	long max = 0, imax;
+	for (i = 0; i < N; i++)
+	{
+		if (req[2*i + 1] >= max)
+		{
+			max = req[2*i + 1] ;
+			imax = i;
+		}
+	}
+	return imax;
+}
+
 int main(int argc, char const *argv[])
 {
 	FILE * fin = fopen("input.in", "r");
@@ -12,8 +26,9 @@ int main(int argc, char const *argv[])
 	
 	long T, N;
 	long i, j;
-	long * req[1000];
+	long req[2000];
 	long out;
+	long nstar;
 	char bad[] = "Too Bad";
 
 	fscanf(fin, "%ld\n", &T);
@@ -23,17 +38,14 @@ int main(int argc, char const *argv[])
 	for (i = 0; i < T; ++i)
 	{
 		fscanf(fin, "%ld\n", &N);
-		printf("%ld\n", N);
-
-		req[0] = calloc(N, sizeof(long) * 2);
-
+		//printf("%ld\n", N);
 
 		for (j = 0; j < N; ++j)
 		{
-			req[j] = req[0] + 2 * j;
-			fscanf(fin, "%ld %ld\n", &req[j][0], &req[j][1]);
-			printf("%ld %ld\n", req[j][0], req[j][1]);
+			fscanf(fin, "%ld %ld\n", &req[2*j], &req[2*j+1]);
+			printf("%ld %ld\n", req[2*j], req[2*j+1]);
 		}
+		printf("%ld\n", findmaxb(req, N));
 
 		//out = mintime(time_string);
 		out = 0;
@@ -47,8 +59,6 @@ int main(int argc, char const *argv[])
 		}
 		else
 			fprintf (fout, "%s", ocase);
-
-		free(req[0]);
 		
 	}
 
