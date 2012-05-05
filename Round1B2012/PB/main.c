72889febf60189b6291518b117e4bb7888cb859a
Haotian Liu <liuhaotian.air@gmail.com>
2012-05-05 12:49:41 -0500

    Init

--- Round1B2012/PB/main.c
+++ Round1B2012/PB/main.c
@@ -5,100 +5,6 @@
 
 long gcd(long a,long b){long c;while(1){c = a % b;if(c == 0)return b;a = b;b = c;}}
 
-long nstar;
-
-long findmaxb(long * req, long N){
-	long i;
-	long max = 0, imax = -1;
-	for (i = 0; i < N; i++)
-	{
-		if (req[4*i + 1] >= max && req[4*i + 1] <= nstar)
-		{
-			if (req[4*i+3] != 0)
-			{
-				continue;
-			}
-			max = req[4*i + 1] ;
-			imax = i;
-		}
-	}
-	return imax;
-}
-
-long findmina(long * req, long N){
-	long i;
-	long max = 0, imin = -1;
-	for (i = 0; i < N; i++)
-	{
-		if (req[4*i + 1] >= max && req[4*i] <= nstar)
-		{
-			if (req[4*i+2] != 0)
-			{
-				continue;
-			}
-			max = req[4*i+1] ;
-			imin = i;
-		}
-	}
-	return imin;
-}
-
-long find2(long * req, long N){
-	long i;
-	long max = 0, imax = -1;
-	for (i = 0; i < N; i++)
-	{
-		if (req[4*i + 1] >= max && req[4*i + 1] <= nstar)
-		{
-			if (req[4*i+3] != 0 || req[4*i+2] != 0)
-			{
-				continue;
-			}
-			max = req[4*i + 1] ;
-			imax = i;
-		}
-	}
-	return imax;
-}
-
-long find1(long * req, long N){
-	long i;
-	long max = 0, imax = -1;
-	for (i = 0; i < N; i++)
-	{
-		if (req[4*i + 1] >= max && req[4*i] <= nstar)
-		{
-			if (req[4*i+3] != 0 || req[4*i+2] != 0)
-			{
-				continue;
-			}
-			max = req[4*i + 1] ;
-			imax = i;
-		}
-	}
-	return imax;
-}
-
-long getstar(long *req, long index){
-	if (index%4 == 0)
-	{
-		req[index + 2] = 1;
-		return 1;
-	}
-	else{
-		if (req[index + 1] == 1)
-		{
-			req[index + 2] = 1;
-			return 1;
-		}
-		else{
-			req[index + 2] = 1;
-			req[index + 1] = 1;
-			return 2;
-		}
-	}
-}
-
 int main(int argc, char const *argv[])
 {
 	FILE * fin = fopen("input.in", "r");
@@ -106,12 +12,8 @@ int main(int argc, char const *argv[])
 	
 	long T, N;
 	long i, j;
-	long req[4000];
-	long star[2000];
 	long out;
-
-	char bad[] = "Too Bad";
-	long tempi;
+	long pvalue[500];
 
 	fscanf(fin, "%ld\n", &T);
 	char ocase[40];
@@ -119,51 +21,19 @@ int main(int argc, char const *argv[])
 
 	for (i = 0; i < T; ++i)
 	{
-		fscanf(fin, "%ld\n", &N);
-		//printf("%ld\n", N);
+		fscanf(fin, "%ld ", &N);
+		//printf("%ld ", N);
 
-		memset(req, 0, sizeof(long)*4000);
-		out = 0;
-		nstar = 0;
-		for (j = 0; j < N; ++j)
+		for (j = 0; j < N - 1; ++j)
 		{
-			fscanf(fin, "%ld %ld\n", &req[4*j], &req[4*j+1]);
-			//printf("%ld %ld\n", req[4*j], req[4*j+1]);
+			fscanf(fin, "%ld ", &pvalue[j]);
+			printf("%ld ", pvalue[j]);
 		}
-		//printf("%ld\n", findmaxb(req, N));
-
-		while(1){
-			tempi = find2(req, N);
-			if (tempi != -1 && req[4*tempi + 1] <= nstar)
-			{
-				nstar += getstar(req, 4*tempi + 1);
-				//printf("2 step:%ld star:%ld\n", tempi, nstar);
-				out++;
-				continue;
-			}
-			tempi = findmaxb(req, N);
-			if (tempi != -1 && req[4*tempi + 1] <= nstar)
-			{
-				nstar += getstar(req, 4*tempi + 1);
-				//printf("b step:%ld star:%ld\n", tempi, nstar);
-				out++;
-				continue;
-			}
-			tempi = findmina(req, N);
-			if (tempi != -1 && req[4*tempi] <= nstar)
-			{
-				nstar += getstar(req, 4*tempi);
-				//printf("a step:%ld star:%ld\n", tempi, nstar);
-				out++;
-				continue;
-			}
-			break;
-		}
+		fscanf(fin, "%ld\n", &pvalue[j]);
+		printf("%ld ", pvalue[j]);
 
 		//out = mintime(time_string);
-		if (nstar != 2*N)
-			sprintf(ocase, "Case #%ld: %s", i + 1, bad);
-		else
+
 		sprintf(ocase, "Case #%ld: %ld", i + 1, out);
 		//printf("%s\n", ocase);
 		
