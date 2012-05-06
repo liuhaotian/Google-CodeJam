a64f794912756afeedd2a8b2c2ba61b6d9e09a2d
Haotian Liu <liuhaotian.air@gmail.com>
2012-05-06 00:42:12 -0500

    Frame for python

---
+++ Round1C2011/PA/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+def main():
+	fin = open("input.in", "r")
+	fout = open("output.out", "w")
+
+	N = int(fin.readline())
+
+	for testcase in range(N):
+		out = testcase
+
+
+
+
+
+
+		print 'Case #%d: %d' % (testcase, out)
+
+if __name__ == '__main__':
+	main()
