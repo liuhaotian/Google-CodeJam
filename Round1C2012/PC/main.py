82c5ad65ef016bdde12c4e54df071b0909bd795f
Haotian Liu <liuhaotian.air@gmail.com>
2012-08-12 11:47:59 -0500

    Init reading

--- Round1C2012/PC/main.py
+++ Round1C2012/PC/main.py
@@ -1,5 +1,14 @@
 def main():
-	pass
+	fin = open('input.txt', 'r')
+	T = int(fin.readline())
+	N, M = fin.readline().split()
+	N = int(N)
+	M = int(M)
+	box = fin.readline().split()
+	box = [ [int(x),int(y)] for x,y in zip(box[0::2],box[1::2])]
+	toy = fin.readline().split()
+	toy = [ [int(x),int(y)] for x,y in zip(toy[0::2],toy[1::2])]
+
 
 if __name__ == '__main__':
 	main()
